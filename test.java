0851dba5321421faf2ed7c81e905540636184a74
eouyang <ericouyang1292@gmail.com>
2014-09-18 03:53:19 -0700

    second

--- test.java
+++ test.java
@@ -2,47 +2,55 @@ import java.io.*;
 import java.util.Scanner;
 
 public class test {
-	public static void main(String[] args)
-	{
-		String reverse;
-		Scanner terminalInput = new Scanner(System.in);
-		System.out.println("Enter the sentence to reverse: ");
-		reverse = terminalInput.nextLine();
-		terminalInput.close();
-		
-		//Reverse the string
-		char[] reverseChar = reverse.toCharArray();
-		for(int i = 0; i < reverse.length()/2; i++)
-		{
-			char temp = reverseChar[i];
-			reverseChar[i] = reverseChar[reverse.length() - 1 - i];
-			reverseChar[reverse.length() - 1 - i] = temp;
-		}
-		
-		//Reverse the token words
-		for(int i = 0; i < reverse.length(); i++)
-		{
-			if(((int)reverseChar[i] >= 65 && (int)reverseChar[i] <= 90) 
-					|| ((int)reverseChar[i] >= 97 && (int)reverseChar[i] <= 122))
-			{
-				//Find the starting and ending indexes
-				int starting = i;
-				while(i < reverse.length() && (((int)reverseChar[i] >= 65 && (int)reverseChar[i] <= 90) 
-					|| ((int)reverseChar[i] >= 97 && (int)reverseChar[i] <= 122)))
-				{
-					i++;
-				}
-				
-				for(int j = 0; j < (i - starting)/2; j++)
-				{
-					char temp = reverseChar[starting + j];
-					reverseChar[starting + j] = reverseChar[i - 1 - j];
-					reverseChar[i - 1 - j] = temp;
-				}
-			}
+    public static void main (String[] args) throws IOException {
+    File file = new File(args[0]);
+    BufferedReader in = new BufferedReader(new FileReader(file));
+    String line;
+    while ((line = in.readLine()) != null) {
+        String[] lineArray = line.split("\n");
+        if (lineArray.length > 0) {
+            //Process line of input Here
+        	for(int m = 0; m <lineArray.length; m++)
+        	{
+        		int strlen = lineArray[m].length();
+        		char reverseChar[] = lineArray[m].toCharArray();
+        		for(int i = 0; i < strlen/2; i++)
+        		{
+        			char temp = reverseChar[i];
+        			reverseChar[i] = reverseChar[strlen - 1 - i];
+        			reverseChar[strlen - 1 - i] = temp;
+        		}
+        		
+        		//Reverse the token words
+        		for(int i = 0; i < strlen; i++)
+        		{
+        			if(((int)reverseChar[i] >= 65 && (int)reverseChar[i] <= 90) 
+        					|| ((int)reverseChar[i] >= 97 && (int)reverseChar[i] <= 122))
+        			{
+        				//Find the starting and ending indexes
+        				int starting = i;
+        				while(i < strlen && (((int)reverseChar[i] >= 65 && (int)reverseChar[i] <= 90) 
+        					|| ((int)reverseChar[i] >= 97 && (int)reverseChar[i] <= 122)))
+        				{
+        					i++;
+        				}
+        				
+        				for(int j = 0; j < (i - starting)/2; j++)
+        				{
+        					char temp = reverseChar[starting + j];
+        					reverseChar[starting + j] = reverseChar[i - 1 - j];
+        					reverseChar[i - 1 - j] = temp;
+        				}
+        			}
 
-		}
-		
-		System.out.println(String.valueOf(reverseChar));
-	}
+        		}
+        		
+        		System.out.println(String.valueOf(reverseChar));
+        		
+        	}
+        }
+    }
+    in.close();
+  }
 }
+
