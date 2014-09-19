929b8534ab3e132cb2e33b4b905675ddf074e014
eouyang <ericouyang1292@gmail.com>
2014-09-19 01:11:45 -0700

    more elegant solution

--- test.java
+++ test.java
@@ -7,47 +7,18 @@ public class test {
     BufferedReader in = new BufferedReader(new FileReader(file));
     String line;
     while ((line = in.readLine()) != null) {
-        String[] lineArray = line.split("\n");
+    	line = line.trim();
+        String[] lineArray = line.split(" ");
         if (lineArray.length > 0) {
             //Process line of input Here
-        	for(int m = 0; m <lineArray.length; m++)
+        	for(int i = lineArray.length - 1; i >= 0; i--)
         	{
-        		int strlen = lineArray[m].length();
-        		char reverseChar[] = lineArray[m].toCharArray();
-        		for(int i = 0; i < strlen/2; i++)
-        		{
-        			char temp = reverseChar[i];
-        			reverseChar[i] = reverseChar[strlen - 1 - i];
-        			reverseChar[strlen - 1 - i] = temp;
-        		}
-        		
-        		//Reverse the token words
-        		for(int i = 0; i < strlen; i++)
-        		{
-        			if(((int)reverseChar[i] >= 65 && (int)reverseChar[i] <= 90) 
-        					|| ((int)reverseChar[i] >= 97 && (int)reverseChar[i] <= 122))
-        			{
-        				//Find the starting and ending indexes
-        				int starting = i;
-        				while(i < strlen && (((int)reverseChar[i] >= 65 && (int)reverseChar[i] <= 90) 
-        					|| ((int)reverseChar[i] >= 97 && (int)reverseChar[i] <= 122)))
-        				{
-        					i++;
-        				}
-        				
-        				for(int j = 0; j < (i - starting)/2; j++)
-        				{
-        					char temp = reverseChar[starting + j];
-        					reverseChar[starting + j] = reverseChar[i - 1 - j];
-        					reverseChar[i - 1 - j] = temp;
-        				}
-        			}
-
-        		}
-        		
-        		System.out.println(String.valueOf(reverseChar));
-        		
+        		if(!lineArray[i].equals(""))
+        			System.out.print(lineArray[i]);
+        		if(i > 0)
+        			System.out.print(" ");
         	}
+        	System.out.println("");
         }
     }
     in.close();
